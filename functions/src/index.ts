b9c85e2e8cb6b74ce2f88fe8afdcb0cd94e13d24
Peter Taylour <peter.taylour@bbc.co.uk>
2019-09-20 16:55:30 +0100

    add first function using express app and slack events

---
+++ functions/src/index.ts
@@ -0,0 +1,30 @@
+import * as functions from "firebase-functions";
+const express = require("express");
+const cors = require("cors");
+const { createEventAdapter } = require("@slack/events-api");
+
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+const slackEvents = createEventAdapter(slackSigningSecret);
+
+interface Event {
+  user: string;
+  channel: string;
+  text: string;
+}
+
+slackEvents.on("message", (event: Event) => {
+  console.log(
+    `Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`
+  );
+});
+
+const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+// Add middleware to listen for slack messages
+app.use("/", slackEvents.requestListener());
+
+// Expose Express API as a single Cloud Function:
+exports.slackEvents = functions.https.onRequest(app);
