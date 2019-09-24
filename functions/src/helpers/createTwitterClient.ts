b17832a26857b0831b481b026703cf8bb585b6e6
Peter Taylour <peter.taylour@bbc.co.uk>
2019-09-24 13:01:05 +0100

    no need to make it lazy

--- functions/src/helpers/createTwitterClient.ts
+++ functions/src/helpers/createTwitterClient.ts
@@ -1,19 +1,12 @@
 import Twit from "twit";
 import * as functions from "firebase-functions";
 
-let client: Twit | null = null;
-
-const createTwitterClient = (config: functions.config.Config): Twit => {
-  if (client === null) {
-    client = new Twit({
-      consumer_key: config.twitter.consumer_api_key,
-      consumer_secret: config.twitter.consumer_api_secret,
-      access_token: config.twitter.access_token,
-      access_token_secret: config.twitter.access_token_secret
-    });
-  }
-
-  return client;
-};
+const createTwitterClient = (config: functions.config.Config): Twit =>
+  new Twit({
+    consumer_key: config.twitter.consumer_api_key,
+    consumer_secret: config.twitter.consumer_api_secret,
+    access_token: config.twitter.access_token,
+    access_token_secret: config.twitter.access_token_secret
+  });
 
 export default createTwitterClient;
