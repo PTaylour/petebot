7000915aadaeeaa9ad05b40cdaf6392172296787
Peter Taylour <peter.taylour@bbc.co.uk>
2019-09-24 12:59:04 +0100

    fill in twitter auth

--- functions/src/helpers/createTwitterClient.ts
+++ functions/src/helpers/createTwitterClient.ts
@@ -6,12 +6,10 @@ let client: Twit | null = null;
 const createTwitterClient = (config: functions.config.Config): Twit => {
   if (client === null) {
     client = new Twit({
-      consumer_key: "...",
-      consumer_secret: "...",
-      access_token: "...",
-      access_token_secret: "...",
-      timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
-      strictSSL: true // optional - requires SSL certificates to be valid.
+      consumer_key: config.twitter.consumer_api_key,
+      consumer_secret: config.twitter.consumer_api_secret,
+      access_token: config.twitter.access_token,
+      access_token_secret: config.twitter.access_token_secret
     });
   }
 
